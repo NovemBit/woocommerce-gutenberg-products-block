66cc3bf774002176f05c8ac2bb85ac4cea014e1b
groguo <eriktadevosyan07@gmail.com>
2021-08-06 17:40:33 +0400

    Fixed Url params attribute part

--- assets/js/base/components/product-list/url-params.js
+++ assets/js/base/components/product-list/url-params.js
@@ -1,7 +1,6 @@
-import {useEffect} from '@wordpress/element';
-import {updateAttributeFilter} from '../../../utils/attributes-query';
-import {useCollection} from "@woocommerce/base-context/hooks";
-import {getSetting} from "@woocommerce/settings";
+import { useEffect } from '@wordpress/element';
+import { useCollection } from '@woocommerce/base-context/hooks';
+import { getSetting } from '@woocommerce/settings';
 
 const params = [
 	'product_cat',
@@ -9,25 +8,28 @@ const params = [
 	'stock_status',
 	'min_price',
 	'max_price',
-	'search'
+	'search',
 ];
 
 let attributesParams = [];
 
-const cleanURLSearchParams = (urlParams) => {
+const cleanURLSearchParams = ( urlParams ) => {
 	let keysForDel = [];
-	urlParams.forEach((v, k) => {
-		k = encodeURIComponent(k)
-		if (v == '' || (k.includes('filter') && !attributesParams.includes(k))) {
-			keysForDel.push(k);
+	urlParams.forEach( ( v, k ) => {
+		k = encodeURIComponent( k );
+		if (
+			v == '' ||
+			( k.includes( 'filter' ) && ! attributesParams.includes( k ) )
+		) {
+			keysForDel.push( k );
 		}
-	});
+	} );
 
-	keysForDel.forEach(k => {
-		urlParams.delete(decodeURIComponent(k));
-	});
+	keysForDel.forEach( ( k ) => {
+		urlParams.delete( decodeURIComponent( k ) );
+	} );
 	return urlParams;
-}
+};
 
 /**
  * @param queryState
@@ -39,138 +41,195 @@ const cleanURLSearchParams = (urlParams) => {
  * @param setMaxPrice
  * @param setSearch
  */
-export const generateUrlParams = (queryState, productAttributes, setProductCat, setStockStatus, setAttributes, setMinPrice, setMaxPrice, setSearch) => {
-	useEffect(() => {
-		const url = new URL(encodeURI(window.location.href));
+export const generateUrlParams = (
+	queryState,
+	productAttributes,
+	setProductCat,
+	setStockStatus,
+	setAttributes,
+	setMinPrice,
+	setMaxPrice,
+	setSearch
+) => {
+	useEffect( () => {
+		const url = new URL( encodeURI( window.location.href ) );
 		let searchParams = url.searchParams;
-		searchParams.forEach((value, key) => {
-			if (value !== null && !key.includes('_qt')) {
-				if (key === 'category') {
-					const CATEGORY_OPTIONS = getSetting('categoryOptions', []);
-					const cats_to_check = value.split(',');
+		searchParams.forEach( ( value, key ) => {
+			if ( value !== null && ! key.includes( '_qt' ) ) {
+				if ( key === 'category' ) {
+					const CATEGORY_OPTIONS = getSetting(
+						'categoryOptions',
+						[]
+					);
+					const cats_to_check = value.split( ',' );
 					let cats_to_set = [];
-					for (const cat_id of cats_to_check) {
-						if (CATEGORY_OPTIONS[cat_id] !== undefined) {
-							cats_to_set.push(cat_id)
+					for ( const cat_id of cats_to_check ) {
+						if ( CATEGORY_OPTIONS[ cat_id ] !== undefined ) {
+							cats_to_set.push( cat_id );
 						}
 					}
-					if( cats_to_set.length > 0 ){
-						searchParams.set('category', cats_to_set)
-					}else{
-						searchParams.delete('category')
+					if ( cats_to_set.length > 0 ) {
+						searchParams.set( 'category', cats_to_set );
+					} else {
+						searchParams.delete( 'category' );
 					}
-					setProductCat(cats_to_set);
-				} else if (key === 'stock_status') {
-					const STOCK_STATUS_OPTIONS = getSetting('stockStatusOptions', []);
-					const status_to_check = value.split(',');
+					setProductCat( cats_to_set );
+				} else if ( key === 'stock_status' ) {
+					const STOCK_STATUS_OPTIONS = getSetting(
+						'stockStatusOptions',
+						[]
+					);
+					const status_to_check = value.split( ',' );
 					let status_to_set = [];
-					for (const status of status_to_check) {
-						if (STOCK_STATUS_OPTIONS[status] !== undefined) {
-							status_to_set.push(status)
+					for ( const status of status_to_check ) {
+						if ( STOCK_STATUS_OPTIONS[ status ] !== undefined ) {
+							status_to_set.push( status );
 						}
 					}
-					if( status_to_set.length > 0 ){
-						searchParams.set('stock_status', status_to_set)
-					}else{
-						searchParams.delete('stock_status')
+					if ( status_to_set.length > 0 ) {
+						searchParams.set( 'stock_status', status_to_set );
+					} else {
+						searchParams.delete( 'stock_status' );
 					}
-					setStockStatus(status_to_set);
-				} else if (key === 'min_price') {
-					setMinPrice(Number(value));
-				} else if (key === 'max_price') {
-					setMaxPrice(Number(value));
-				} else if (key === 's') {
-					setSearch(decodeURIComponent(value))
+					setStockStatus( status_to_set );
+				} else if ( key === 'min_price' ) {
+					setMinPrice( Number( value ) );
+				} else if ( key === 'max_price' ) {
+					setMaxPrice( Number( value ) );
+				} else if ( key === 's' ) {
+					setSearch( decodeURIComponent( value ) );
 				}
 			}
-		});
+		} );
 		url.search = searchParams.toString();
-		const new_url = decodeURI(url.toString());
-		if (new_url !== window.location.href) {
-			window.history.pushState("", "", new_url.replaceAll('%2C', ','));
+		const new_url = decodeURI( url.toString() );
+		if ( new_url !== window.location.href ) {
+			window.history.pushState(
+				'',
+				'',
+				new_url.replaceAll( '%2C', ',' )
+			);
 		}
-	}, []);
+	}, [] );
 
-	const {results, isLoading} = useCollection({
+	const { results, isLoading } = useCollection( {
 		namespace: '/wc/store',
 		resourceName: 'products/attributes',
-		resourceValues: []
-	});
+		resourceValues: [],
+	} );
 
-	useEffect(() => {
-		if (isLoading) {
+	useEffect( () => {
+		if ( isLoading ) {
 			return;
 		}
-		const url = new URL(window.location.href);
+		const url = new URL( window.location.href );
 		let searchParams = url.searchParams;
 		let query = [];
-		searchParams.forEach((value, key) => {
-			if (value !== null && !key.includes('_qt')) {
-				if (key !== 'category' && key !== 'stock_status' && key !== 'min_price' && key !== 'max_price' && key !== 's' && key !== 'post_type') {
-					let attributeObject = {};
+		searchParams.forEach( ( value, key ) => {
+			if (
+				value !== null &&
+				! key.includes( '_qt' ) &&
+				key.includes( 'filter' )
+			) {
+				let attributeObject = {};
 
-					let termsSlugs = value.split(',');
-					let term, terms = [];
-					for (let i = 0; i < termsSlugs.length; i++) {
-						term = encodeURIComponent(termsSlugs[i]).toLowerCase();
-						terms.push(term)
-					}
-					attributeObject['attribute'] = key.replace('filter_', 'pa_');
-					attributeObject['operator'] = searchParams.get(key + '_qt');
-					attributeObject['slug'] = terms;
-					query.push(attributeObject);
+				let termsSlugs = value.split( ',' );
+				let term,
+					terms = [];
+				for ( let i = 0; i < termsSlugs.length; i++ ) {
+					term = encodeURIComponent( termsSlugs[ i ] ).toLowerCase();
+					terms.push( term );
 				}
+				attributeObject[ 'attribute' ] = key.replace(
+					'filter_',
+					'pa_'
+				);
+				attributeObject[ 'operator' ] = searchParams.get( key + '_qt' );
+				attributeObject[ 'slug' ] = terms;
+				query.push( attributeObject );
 			}
-		});
-		setAttributes(query);
-	}, [results, isLoading]);
+		} );
+		setAttributes( query );
+	}, [ results, isLoading ] );
 
-	useEffect(() => {
-		if (isLoading) {
+	useEffect( () => {
+		if ( isLoading ) {
 			return;
 		}
-		const url = new URL(window.location.href);
+		const url = new URL( window.location.href );
 		let searchParams = url.searchParams;
-		if (queryState.min_price === undefined) {
-			searchParams.delete('min_price');
+		if ( queryState.min_price === undefined ) {
+			searchParams.delete( 'min_price' );
 		}
-		if (queryState.max_price === undefined) {
-			searchParams.delete('max_price');
+		if ( queryState.max_price === undefined ) {
+			searchParams.delete( 'max_price' );
 		}
-		for (const queryStateElement in queryState) {
-			if (params.includes(queryStateElement)) {
+		for ( const queryStateElement in queryState ) {
+			if ( params.includes( queryStateElement ) ) {
 				//set url params
-				if (queryStateElement === 'attributes') {
+				if ( queryStateElement === 'attributes' ) {
 					attributesParams = [];
-					for (const attribute of queryState[queryStateElement]) {
-						let to_push_attr = attribute.attribute.replace('pa_', 'filter_');
-						let encoded_to_push_attr = encodeURIComponent(to_push_attr);
+					for ( const attribute of queryState[ queryStateElement ] ) {
+						let to_push_attr = attribute.attribute.replace(
+							'pa_',
+							'filter_'
+						);
+						let encoded_to_push_attr = encodeURIComponent(
+							to_push_attr
+						);
 						//attribute
-						if (!attributesParams.includes(encoded_to_push_attr)) {
-							attributesParams.push(encoded_to_push_attr);
+						if (
+							! attributesParams.includes( encoded_to_push_attr )
+						) {
+							attributesParams.push( encoded_to_push_attr );
 						}
-						if (!attributesParams.includes(encoded_to_push_attr + '_qt')) {
-							attributesParams.push(encoded_to_push_attr + '_qt');
+						if (
+							! attributesParams.includes(
+								encoded_to_push_attr + '_qt'
+							)
+						) {
+							attributesParams.push(
+								encoded_to_push_attr + '_qt'
+							);
 						}
-						searchParams.set(to_push_attr, decodeURIComponent(attribute.slug).toLowerCase())
-						searchParams.set(to_push_attr + '_qt', attribute.operator)
+						searchParams.set(
+							to_push_attr,
+							decodeURIComponent( attribute.slug ).toLowerCase()
+						);
+						searchParams.set(
+							to_push_attr + '_qt',
+							attribute.operator
+						);
 					}
 				} else {
-					if (queryStateElement === 'product_cat') {
-						searchParams.set('category', queryState[queryStateElement])
-					} else if (queryStateElement === 'search') {
-						searchParams.set('s', queryState[queryStateElement])
+					if ( queryStateElement === 'product_cat' ) {
+						searchParams.set(
+							'category',
+							queryState[ queryStateElement ]
+						);
+					} else if ( queryStateElement === 'search' ) {
+						searchParams.set(
+							's',
+							queryState[ queryStateElement ]
+						);
 					} else {
-						searchParams.set(queryStateElement, queryState[queryStateElement])
+						searchParams.set(
+							queryStateElement,
+							queryState[ queryStateElement ]
+						);
 					}
 				}
-				searchParams = cleanURLSearchParams(searchParams);
+				searchParams = cleanURLSearchParams( searchParams );
 				url.search = searchParams.toString();
 			}
 		}
-		const new_url = decodeURI(url.toString());
-		if (new_url !== window.location.href) {
-			window.history.pushState("", "", new_url.replaceAll('%2C', ','));
+		const new_url = decodeURI( url.toString() );
+		if ( new_url !== window.location.href ) {
+			window.history.pushState(
+				'',
+				'',
+				new_url.replaceAll( '%2C', ',' )
+			);
 		}
-	}, Object.values(queryState));
-}
+	}, Object.values( queryState ) );
+};
